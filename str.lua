c68a1b0c50df9691eadc6e4394af2bdbfdda1685
ebayboy <ebayboy@163.com>
2020-08-14 11:13:32 +0800

    commit for

--- str.lua
+++ str.lua
@@ -2,8 +2,14 @@
 
 local a = [["a", "b", "c"]]
 local b = [=[[0-9]]=]
+local c = "name"
 
 print (a)
 
 print (b)
+
+print("c:" .. c)
+
+print("# len:" .. #a)
+print("# string.len(a):" .. string.len(a))
 
---
+++ test/for.lua
@@ -0,0 +1,11 @@
+
+for i=0, 10, 1 do
+	print("i:".. i)
+end
+
+
+a = {"aa", "bb", "cc", "dd"}
+
+for i, v in ipairs(a) do
+	print("index:" .. i .. "value:".. v)
+end
